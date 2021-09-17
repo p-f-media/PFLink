3da1e8029bc25501e5ba2ec17d816802f4f770f9
momii_daiji <momii.daiji@vxv.co.jp>
2021-09-17 16:02:56 +0900

    バージョン検証追加

--- PFLink/PFLinkSamples/SampleScene01/Scripts/CallTestView.cs
+++ PFLink/PFLinkSamples/SampleScene01/Scripts/CallTestView.cs
@@ -5,6 +5,8 @@ using PFLink;
 
 public class CallTestView : MonoBehaviour
 {
+    [SerializeField] string ver = "0.1.0";
+    [SerializeField] Text verText = null;
     [SerializeField] string param = "?param=1234";
     [SerializeField] string packageName = "";
     [SerializeField] string androidStoreURL = "https://play.google.com/store/apps/details?id=";
@@ -12,7 +14,7 @@ public class CallTestView : MonoBehaviour
 
     void Start()
     {
-        
+        if( verText != null ) verText.text = ver;
     }
 
     public void OnSimpleDeepLinkButtonClicked()
